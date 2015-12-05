55347e48b1857e3e842b2c58b9f1b843ee7e78d4
William Bajzek <williambajzek@gmail.com>
2015-12-05 12:47:16 -0600

    initial specs

--- spec/launchpad_step_sequencer_spec.rb
+++ spec/launchpad_step_sequencer_spec.rb
@@ -1,11 +1,44 @@
 require 'spec_helper'
 
 describe LaunchpadStepSequencer do
+  subject { described_class.new }
+
   it 'has a version number' do
     expect(LaunchpadStepSequencer::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  context 'when started' do
+    xit 'starts on the first step' do
+    end
+
+    xit 'turns on the step number lights' do
+    end
+
+    xit 'highlights the first step number light' do
+    end
+
+    xit 'starts any selected notes' do
+    end
+
+    context 'when advanced' do
+      xit 'unhighlights the first step number light' do
+      end
+
+      xit 'highlights the second step number light' do
+      end
+
+      xit 'stops any unselected notes' do
+      end
+
+      context 'from the last step' do
+        xit 'returns to the first step' do
+        end
+      end
+    end
+
+    context 'when stopped' do
+      xit 'unhighlights the current step number light' do
+      end
+    end
   end
 end
